4221ec086e3033c0dc340bcd91db365a12f9834f
Júnior Carvalho <joseadolfojr@gmail.com>
2016-02-01 15:22:46 -0200

    habilita inscricao

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -12,3 +12,7 @@ class HomeTest(TestCase):
     def test_template(self):
         """Must use index.html"""
         self.assertTemplateUsed(self.response, "index.html")
+
+    def test_subscription_linl(self):
+        self.assertContains(self.response, 'href="/inscricao/')
+
