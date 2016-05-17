54612adf9b9f29378a8f43860d6ae705c9117581
Júnior Carvalho <joseadolfojr@gmail.com>
2016-05-17 12:07:39 -0300

    customiza pagina de erro

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -3,8 +3,12 @@ from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
 
 class SubscribeTest(TestCase):
+
+
     def setUp(self):
         self.resp = self.client.get('/inscricao/')
+
+
     def test_get(self):
         """Get /inscricao/ must return status code 200"""
         self.assertEqual(200, self.resp.status_code)
