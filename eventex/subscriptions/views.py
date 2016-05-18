847a2d2c0d4e15471d54cb5f3e09a72f8eba7e81
Júnior Carvalho <joseadolfojr@gmail.com>
2016-05-18 16:35:08 -0300

    gravando na base

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 def subscribe(request):
@@ -21,11 +22,13 @@ def create(request):
         return render(request, 'subscriptions/subscription_form.html',
                       {'form': form})
 
-    _send_mail('Confirmação de inscrição',
-               settings.DEFAULT_FROM_EMAIL,
-               form.cleaned_data['email'],
-               'subscriptions/subscription_email.txt',
-               form.cleaned_data)
+    #_send_mail('Confirmação de inscrição',
+    #           settings.DEFAULT_FROM_EMAIL,
+    #           form.cleaned_data['email'],
+    #           'subscriptions/subscription_email.txt',
+    #           form.cleaned_data)
+
+    Subscription.objects.create(**form.cleaned_data)
 
     messages.success(request, 'Inscrição realizada com sucesso!')
 
