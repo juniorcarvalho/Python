3007d219c80c561b4d3fa349c7becf4254b8c31e
Júnior Carvalho <joseadolfojr@gmail.com>
2016-05-19 14:53:35 -0300

    view agrecedimento inscricao

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -46,7 +46,7 @@ class SubscribePostValid(TestCase):
 
     def test_post(self):
         """Valid post should redirect to /inscricao/"""
-        self.assertEqual(302, self.resp.status_code)
+        self.assertRedirects(self.resp, '/inscricao/1/')
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
@@ -76,11 +76,3 @@ class SubscribePostInvalid(TestCase):
 
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
-
-
-class SubscribeSuccessMessage(TestCase):
-    def test_message(self):
-        data = dict(name='Junior Carvalho', cpf='04539428659',
-                    email='joseadolfojr@gmail.com', phone='21996016875')
-        response = self.client.post('/inscricao/', data, follow=True)
-        self.assertContains(response, 'Inscrição realizada com sucesso!')
