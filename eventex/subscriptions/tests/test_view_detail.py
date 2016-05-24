021ecaf48990b97e0a3119c9d412b592dc2a4677
Júnior Carvalho <joseadolfojr@gmail.com>
2016-05-24 17:12:58 -0300

    Extrai url

--- eventex/subscriptions/tests/test_view_detail.py
+++ eventex/subscriptions/tests/test_view_detail.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from eventex.subscriptions.models import Subscription
+from django.shortcuts import resolve_url as r
 
 
 class SubscriptionDetailGet(TestCase):
@@ -10,7 +11,7 @@ class SubscriptionDetailGet(TestCase):
             email='joseadolfojr@gmail.com',
             phone='21996016875'
         )
-        self.resp = self.client.get('/inscricao/{}/'.format(self.obj.pk))
+        self.resp = self.client.get(r('subscriptions:detail', self.obj.pk))
 
     def test_get(self):
         self.assertEqual(200, self.resp.status_code)
@@ -27,10 +28,10 @@ class SubscriptionDetailGet(TestCase):
 
         with self.subTest():
             for expected in contents:
-                self.assertContains(self.resp, expected )
+                self.assertContains(self.resp, expected)
 
 
 class SubscriptionDetailGetNotFound(TestCase):
     def test_not_found(self):
-        resp = self.client.get('/inscricao/0/')
+        resp = self.client.get(r('subscriptions:detail', 0))
         self.assertEqual(404, resp.status_code)
