6ccceba6e7c5ba6b42300edead345a09bcb4c2de
Júnior Carvalho <joseadolfojr@gmail.com>
2016-06-15 18:17:27 -0300

    commid

--- eventex/urls.py
+++ eventex/urls.py
@@ -3,12 +3,8 @@ from django.contrib import admin
 from eventex.core.views import home
 
 urlpatterns = [
-<<<<<<< HEAD
-    url(r'^$', home),
-=======
     url(r'^$',home, name='home'),
     url(r'^inscricao/', include('eventex.subscriptions.urls',
                                 namespace='subscriptions')),
->>>>>>> 1731772b53b6fe6f7060d384d410dd85f8a90e07
     url(r'^admin/', admin.site.urls),
 ]
