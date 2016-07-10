afcb43bf67d985436c123dea484a9ba1bd813127
Júnior Carvalho <joseadolfojr@gmail.com>
2016-07-10 01:52:35 -0300

    Mixins

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from eventex.subscriptions.validators import validate_cpf
+from django.shortcuts import resolve_url as r
 
 
 class Subscription(models.Model):
@@ -17,3 +18,6 @@ class Subscription(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return r('subscriptions:detail', self.pk)
